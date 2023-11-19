66ab5fe1ac60d4fd85b43677303f5772bd4db9bf
Nicole Slater <nicoleslater@pursuit.org>
2023-11-19 12:48:57 -0500

    Many to Many tables created, checking keys and References

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -5,6 +5,14 @@ CREATE DATABASE lovelysoaps_dev;
 
 \c lovelysoaps_dev;
 
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY, 
+    first_name VARCHAR(50),
+    last_name VARCHAR(50), 
+    email VARCHAR(255),
+    product_id INTEGER REFERENCES products (id)
+    ON DELETE CASCADE
+);
 
 
 CREATE TABLE categories (
@@ -19,5 +27,7 @@ CREATE TABLE products (
     price NUMERIC, 
     quantity_in_stock BOOLEAN, 
     category_id INTEGER REFERENCES categories (id)
-    ON DELETE CASCADE 
+    ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users (id)
+    ON DELETE CASCADE
 );
