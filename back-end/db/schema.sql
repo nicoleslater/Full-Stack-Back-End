f7c789a35926e1807dd8a9744687103aa5306de2
Nicole Slater <nicoleslater@pursuit.org>
2023-11-19 14:30:08 -0500

    Updated schema.sql, looking at CSS, HTML features to use for the front end

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,9 +1,25 @@
 DROP DATABASE IF EXISTS lovelysoaps_dev;
 
-
 CREATE DATABASE lovelysoaps_dev;
 
 \c lovelysoaps_dev;
+
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY, 
+    name TEXT, 
+);
+
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY, 
+    name TEXT NOT NULL, 
+    description TEXT, 
+    price NUMERIC, 
+    _in_stock BOOLEAN, 
+    category_id INTEGER REFERENCES categories(category_id)
+    ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(user_id)
+    ON DELETE CASCADE, 
+);
 
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY, 
@@ -29,22 +45,5 @@ CREATE TABLE order_items (
     product_id INTEGER REFERENCES products(product_id)
     ON DELETE CASCADE,
     quantity INTEGER
-)
-
-
-CREATE TABLE categories (
-    category_id SERIAL PRIMARY KEY, 
-    name TEXT, 
 );
 
-CREATE TABLE products (
-    product_id SERIAL PRIMARY KEY, 
-    name TEXT NOT NULL, 
-    description TEXT, 
-    price NUMERIC, 
-    quantity_in_stock BOOLEAN, 
-    category_id INTEGER REFERENCES categories(category_id)
-    ON DELETE CASCADE,
-    user_id INTEGER REFERENCES users(user_id)
-    ON DELETE CASCADE, 
-);
