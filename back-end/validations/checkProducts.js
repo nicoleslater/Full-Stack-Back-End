9a495fb5ccfd20542334acff1941a43195497a27
Nicole Slater <nicoleslater@pursuit.org>
2023-11-21 22:43:13 -0500

    Updated validations Folder with checkProducts.js

---
+++ back-end/validations/checkProducts.js
@@ -0,0 +1,21 @@
+const checkName = (req, res, next) => {
+    if(req.body.name){
+        console.log("Name is OK")
+        next()
+    } else{
+        res.status(400).json( { error: "Name is required!"})
+    }
+}
+
+const checkBoolean = (req, res, next) => {
+    if(req.body._in_stock === true || req.body._in_stock === false){
+        next()
+    } else{
+        res.status(400).json( { error: "_in_stock must be a boolean value"})
+    }
+}
+
+module.exports = checkName = {
+    checkName, 
+    checkBoolean
+}
