a3960ff78ab81def2ef9f1dc4ee78259a7f86e45
Nicole Slater <nicoleslater@pursuit.org>
2023-11-22 20:01:23 -0500

    Updating Seed SQL

---
+++ back-end/db/seed.sql
@@ -0,0 +1,15 @@
+\c lovelysoaps;
+
+INSERT INTO products (product_id, name, description, price, _in_stock, user_id);
+
+INSERT INTO users (first_name, last_name, email)
+VALUES 
+('Sharon', 'Jones', 'sharon.jones@gmail.com'),
+('Autumn', 'Stevens', 'stevenssavvy@gmail.com'),
+('Caleb', 'Davis', 'davisC@gmail.com');
+
+INSERT INTO orders ()
+VALUES 
+(),
+(),
+();
