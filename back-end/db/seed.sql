1e9dad6aaf3d0ec3f08984611d28393d5e535523
Nicole Slater <nicoleslater@pursuit.org>
2023-11-25 02:01:23 -0500

    Adding more fields to the tables

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -6,11 +6,11 @@ VALUES
 ('Green Tea Face Scrub', 'Nourshing and exfoliating that is gentle enough for sensitive skin', 999, true, 2),
 ('Almond Honey Soap', 'Conditions the skin with leaving the skin soft', 1599, false, 3);
 
-INSERT INTO users (first_name, last_name, email)
+INSERT INTO users (first_name, last_name, email, shipping_address, preferred_delivery)
 VALUES 
-('Sharon', 'Jones', 'sharon.jones@gmail.com'),
-('Autumn', 'Stevens', 'stevenssavvy@gmail.com'),
-('Caleb', 'Davis', 'davisC@gmail.com');
+('Sharon', 'Jones', 'sharon.jones@gmail.com', '789 Elm St, Smallburg, TX, 83201', true),
+('Autumn', 'Stevens', 'stevenssavvy@gmail.com', '456 Oak Rd, Suburbatoon, OR, 97123', true),
+('Caleb', 'Davis', 'davisC@gmail.com', '352 Finley Parkway Apt 5E, East Lydia, NY 12345', false);
 
 INSERT INTO orders (user_id, order_date, total_price, delivery, pick_up)
 VALUES 
