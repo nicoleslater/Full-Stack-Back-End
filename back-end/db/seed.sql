ac981bf8618c151e6f2850db31940c5a0db0dd2f
Nicole Slater <nicoleslater@pursuit.org>
2023-11-25 01:46:00 -0500

    Updating SQL and SEED

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,6 +1,10 @@
 \c lovelysoaps;
 
-INSERT INTO products (product_id, name, description, price, _in_stock, user_id);
+INSERT INTO products (name, description, price, _in_stock, user_id)
+VALUES 
+('Cocoa Butter Kisses', 'Perfect lotion for Winter Season and leaves the skin moisturized', 1899, true, 1 ),
+('Green Tea Face Scrub', 'Nourshing and exfoliating that is gentle enough for sensitive skin', 999, true, 2),
+('Almond Honey Soap', 'Conditions the skin with leaving the skin soft', 1599, false, 3);
 
 INSERT INTO users (first_name, last_name, email)
 VALUES 
@@ -8,7 +12,7 @@ VALUES
 ('Autumn', 'Stevens', 'stevenssavvy@gmail.com'),
 ('Caleb', 'Davis', 'davisC@gmail.com');
 
-INSERT INTO orders ()
+INSERT INTO orders (user_id, order_date, total_price)
 VALUES 
 (),
 (),
