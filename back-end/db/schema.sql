4046288745049ceed3c322a0a4f45e6e6d6a952b
Nicole Slater <nicoleslater@pursuit.org>
2023-11-27 13:25:56 -0500

    Reordering tables for DB

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -4,14 +4,12 @@ CREATE DATABASE lovelyskincare_dev;
 
 \c lovelyskincare_dev;
 
-CREATE TABLE products (
-    product_id SERIAL PRIMARY KEY, 
-    name TEXT NOT NULL, 
-    description TEXT, 
-    price NUMERIC, 
-    _in_stock BOOLEAN, 
-    ingredients TEXT, 
-    category TEXT, 
+CREATE TABLE orders ( 
+    order_id SERIAL PRIMARY KEY, 
+    order_date DATE, 
+    total_price NUMERIC(10, 2),
+    delivery BOOLEAN, 
+    pick_up BOOLEAN, 
     user_id INTEGER REFERENCES users(id)
     ON DELETE CASCADE
 );
@@ -27,12 +25,14 @@ CREATE TABLE users (
     ON DELETE CASCADE 
 );
 
-CREATE TABLE orders ( 
-    order_id SERIAL PRIMARY KEY, 
-    order_date DATE, 
-    total_price NUMERIC(10, 2),
-    delivery BOOLEAN, 
-    pick_up BOOLEAN, 
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY, 
+    name TEXT NOT NULL, 
+    description TEXT, 
+    price NUMERIC, 
+    _in_stock BOOLEAN, 
+    ingredients TEXT, 
+    category TEXT, 
     user_id INTEGER REFERENCES users(id)
     ON DELETE CASCADE
 );
