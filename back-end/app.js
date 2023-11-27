d1cd1482bbfbeeff34d37d58d1467988fc3c415c
Nicole Slater <nicoleslater@pursuit.org>
2023-11-27 16:36:41 -0500

    Debugging the server, status OK, Db created

--- back-end/app.js
+++ back-end/app.js
@@ -4,32 +4,20 @@ const cors = require("cors");
 const app = express();
 
 const usersController = require("./controllers/usersController");
-const ordersController = require("./controllers/ordersController");
-const productsController = require("./controllers/productsController");
+// const ordersController = require("./controllers/ordersController");
+// const productsController = require("./controllers/productsController");
 
 app.use(cors());
 app.use(express.json());
 
 
 app.use("/users", usersController);
-app.use("/orders", ordersController);
-app.use("/products", productsController);
+// app.use("/orders", ordersController);
+// app.use("/products", productsController);
 
 
 app.get("/", (req, res) => {
     res.send("Welcome to Lovely Nikki Soaps");
-});
-
-app.get("/users", (req, res) => {
-    res.send("Welcome to User Profile");
-});
-
-app.get("/orders", (req, res) => {
-    res.send("Welome to the Orders");
-});
-
-app.get("/products", (req, res) => {
-    res.send("Welcome to the Products");
 });
 
 app.get("*", (req, res) => {
