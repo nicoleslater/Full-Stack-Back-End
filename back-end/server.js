2c11e6e0e4ad460726ddaa029cb233e40133e764
Nicole Slater <nicoleslater@pursuit.org>
2023-11-27 15:03:29 -0500

    Debugging server, updating app.js with controllers and require

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,9 @@
-const app = require("./app");
+const app = require("./app.js");
 
 require("dotenv").config();
+
 const PORT = process.env.PORT || 5566;
 app.listen(PORT, () => {
     console.log(`Products live on port: ${PORT}`)
-})
+});
+
