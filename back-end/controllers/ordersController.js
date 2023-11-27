de7715b159e2fe7299c7da211d13477f1234b542
Nicole Slater <nicoleslater@pursuit.org>
2023-11-27 14:54:19 -0500

    Updated Users Controllers

--- back-end/controllers/ordersController.js
+++ back-end/controllers/ordersController.js
@@ -8,3 +8,62 @@ const {
     updateOrder
 } = require("../queries/orders");
 
+const { checkName, checkBoolean } = require("../validations/checkOrders.js");
+
+const orders = express.Router();
+
+orders.get(":/id", async (req, res) => {
+    const { id } = req.params;
+    const oneOrder = await getOneOrder(id)
+    if(oneOrder){
+        res.json(oneOrder)
+    } else{
+        res.status(404).json({ error: "Sorry that order is not found!"});
+    }
+});
+
+orders.get("/", async (req, res) => {
+    const allOrders = await getAllOrders();
+    if(allOrders[0]){
+        res.status(200)
+        .json( { success: true, data: { payload: allOrders }});
+    } else {
+        res.status(500)
+        .json({ success: false, data: { error: "Error with the Server, please try again!"}});
+    }
+});
+
+orders.post("/", checkName, checkBoolean, async (req, res) => {
+    try{
+        const createdOrder = await createOrder(req.body);
+        res.json(createdOrder)
+    } catch(error){
+        res.status(400).json({ error: "Please go Back there is a server error!"});
+    }
+});
+
+orders.delete("/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+        const deletedOrder = await deleteOrder(id);
+        if(deletedOrder){
+            res.status(200).json({ success: true, payload: { data: deletedOrder }})
+        } else {
+            res.status(404).json("Sorry that order is not found!");
+        }
+    } catch(err){
+        res.send(err)
+    }
+});
+
+orders.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const updatedOrder = await updateOrder(id, req.body);
+    if(updatedOrder.id){
+        res.status(200).json(updatedOrder);
+    } else{
+        res.status(404).json("NO Order found with that ID");
+    }
+});
+
+module.exports = orders;
--- back-end/controllers/usersController.js
+++ back-end/controllers/usersController.js
@@ -10,4 +10,60 @@ const {
 
 const users = express.Router();
 
-const { checkName, checkBoolean } = require("../validations/check")
+const { checkName, checkBoolean } = require("../validations/checkUsers");
+
+users.get(":/id", async (req, res) => {
+    const { id } = req.params;
+    const oneUser = await getOneUser(id)
+    if(oneUser){
+        res.json(oneUser)
+    } else{
+        res.status(404).json({ error: "Sorry that user is not found!"});
+    }
+});
+
+users.get("/", async (req, res) => {
+    const allUsers = await getAllUsers();
+    if(allUsers[0]){
+        res.status(200)
+        .json( { success: true, data: { payload: allUsers }});
+    } else {
+        res.status(500)
+        .json({ success: false, data: { error: "Error with the Server, please try again!"}});
+    }
+});
+
+users.post("/", checkName, checkBoolean, async (req, res) => {
+    try{
+        const createdUser = await createUser(req.body);
+        res.json(createdUser)
+    } catch(error){
+        res.status(400).json({ error: "Please go Back there is a server error!"});
+    }
+});
+
+users.delete("/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+        const deletedUser = await deleteUser(id);
+        if(deletedUser){
+            res.status(200).json({ success: true, payload: { data: deletedUser }})
+        } else {
+            res.status(404).json("Sorry that user is not found!");
+        }
+    } catch(err){
+        res.send(err)
+    }
+});
+
+users.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const updatedUser = await updateUser(id, req.body);
+    if(updatedUser.id){
+        res.status(200).json(updatedUser);
+    } else{
+        res.status(404).json("NO user found with that ID");
+    }
+});
+
+module.exports = users;
