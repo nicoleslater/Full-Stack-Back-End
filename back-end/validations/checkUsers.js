5e4aeec118c5474c13dcadc1d4c3514aeacf073c
Nicole Slater <nicoleslater@pursuit.org>
2023-12-02 22:49:21 -0500

    Adding Users back to db

---
+++ back-end/validations/checkUsers.js
@@ -0,0 +1,22 @@
+const checkName = (req, res, next) => {
+    if(req.body.name){
+        console.log("Name is OK")
+        next()
+    } else{
+        res.status(400).json({ error: "Name is required!"})
+    }
+    
+}
+
+const checkBoolean = (req, res, next) => {
+    if(req.body.preferred_delivery === true || req.body.preferred_delivery === false){
+        next()
+    } else{
+        res.status(400).json({ error: "preferred_delivery must be a boolean value"})
+    }
+}
+
+module.exports = {
+    checkName, 
+    checkBoolean
+}
