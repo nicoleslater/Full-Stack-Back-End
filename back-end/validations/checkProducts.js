4244dde7c042abf8d1292348f7eb2cd024c4c4e1
Nicole Slater <nicoleslater@pursuit.org>
2023-12-17 09:45:46 -0500

    Updating Validations for checkProducts.js

--- back-end/validations/checkProducts.js
+++ back-end/validations/checkProducts.js
@@ -3,7 +3,7 @@ const checkName = (req, res, next) => {
         console.log("Name is OK")
         next()
     } else{
-        res.status(400).json( { error: "Name is required!"})
+        res.status(400).json( { error: "Name is required!"} )
     }
 }
 
@@ -11,7 +11,7 @@ const checkBoolean = (req, res, next) => {
     if(req.body.stocked === true || req.body.stocked === false){
         next()
     } else{
-        res.status(400).json( { error: "stocked must be a boolean value"})
+        res.status(400).json( { error: "stocked must be a boolean value"} )
     }
 }
 
