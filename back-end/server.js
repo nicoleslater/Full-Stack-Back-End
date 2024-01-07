371d6320ddfe7a9d55becbc4b287b23e78729afa
Nicole Slater <nicoleslater@pursuit.org>
2024-01-07 06:19:52 -0500

    updating Users Controlller

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,10 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5566;
 app.listen(PORT, () => {
-    console.log(`Users live on port: ${PORT}`)
+    console.log(`
+    Users live on port: ${PORT}
+    Products live on port: ${PORT}
+    Orders live on port: ${PORT}`
+    )
 });
 
