89f1f38365115fce722ed8716026aaa3b3441269
Nicole Slater <nicoleslater@pursuit.org>
2024-01-22 03:52:14 -0500

    Updating usersController to make CRUD is in motion

--- back-end/controllers/usersController.js
+++ back-end/controllers/usersController.js
@@ -14,46 +14,48 @@ const users = express.Router();
 
 // Index
 users.get(":/id", async (req, res) => {
-    const { id } = req.params;
-    const oneUser = await getOneUser(id);
-    if(oneUser){
+    const { id } = req.params
+    const oneUser = await getOneUser(id)
+    if (oneUser){
         res.json(oneUser)
-    } else{
-        res.status(404).json({ error: "Sorry that user is not found!"});
+    } else {
+        res.status(404).json({error: "User with that ID is NOT found!"})
     }
 });
 
 // Show
 users.get("/", async (req, res) => {
     const allUsers = await getAllUsers();
-    if(allUsers[0]){
-        res.status(200).json({ success: true, data: { payload: allUsers }});
-    } else{
-        res.status(404).json({ success: false, data: { error: "Error (User Controller) with the Server, please try again!" } });
+    if (allUsers[0]) {
+        res.status(200)
+        .json({ success: true, data: { payload: allUsers } });
+    } else {
+        res.status(500)
+        .json({ success: false, data: { error: "Server Error!" } });
     }
 });
 
 // Post
 users.post("/", checkName, checkBoolean, async (req, res) => {
     try{
-        const createdUser = await createUser(req.body);
+        const createdUser = await createUser(req.body)
         res.json(createdUser)
-    } catch(error){
-        res.status(404).json({ error: "Please go Back (UserController) there is a server error!"});
+    } catch (error) {
+        res.status(400).json({error: "Show is NOT Available please come back to Users Controller!"})
     }
-});
+})
 
 // Delete
 users.delete("/:id", async (req, res) => {
-    try{
+    try {
         const { id } = req.params;
         const deletedUser = await deleteUser(id);
-        if(deletedUser){
-            res.status(200).json({ success: true, payload: { data: deletedUser } })
+        if(deletedUser) {
+            res.status(200).json({ success:true, payload: { data: deletedUser } })
         } else {
-            res.status(404).json("Sorry that user is not found!");
+            res.status(404).json("User Not Found! Please go back to Delete(UsersController)")
         }
-    } catch(err){
+    } catch(err) {
         res.send(err)
     }
 });
@@ -61,12 +63,12 @@ users.delete("/:id", async (req, res) => {
 // Update
 users.put("/:id", async (req, res) => {
     const { id } = req.params;
-    const updatedUser = await updateUser( {id, ...req.body} );
-    if(updatedUser.id){
+    const updatedUser = await updateUser(id, req.body);
+    if(updatedUser.id) {
         res.status(200).json(updatedUser);
-    } else{
-        res.status(404).json("NO user found with that ID");
-    }
-});
+    } else (
+        res.status(404).json("Sorry no User with that ID!")
+    )
+})
 
 module.exports = users;
